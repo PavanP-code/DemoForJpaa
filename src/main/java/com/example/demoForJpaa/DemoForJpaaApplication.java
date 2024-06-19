f79a1bb9c94914084d0cd7060c975a34d8e2a0e6
Pavan <pavanpenta3@outlook.com>
2024-06-19 11:20:53 +0530

    first commit

---
+++ src/main/java/com/example/demoForJpaa/DemoForJpaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoForJpaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForJpaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoForJpaaApplication.class, args);
+	}
+
+}
